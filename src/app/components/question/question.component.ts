eff404424ea691e3984afb99a9fcc96753cb9f99
Tobiasz <tobijas.ch@gmail.com>
2024-02-02 09:03:49 +0100

    create service and exdend component in question

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QuestionService, Questions } from './question.service';
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
   styleUrls: ['./question.component.css']
 })
-export class QuestionComponent {
+export class QuestionComponent implements OnInit{
 
+  questions: Questions[] = [];
+
+  constructor(private questionService :QuestionService) {
+    
+  }
+  ngOnInit(): void {
+    this.questionService.getQuestions().subscribe(value => value = this.questions );
+  }
 }
